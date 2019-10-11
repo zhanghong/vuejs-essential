3b4de4b76d6a3c7199b4b4cc91f36bde89fca14f
zhanghongbean <zhanghong_bean@126.com>
2019-10-11 13:56:53 +0800

    注册表单验证

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 // 如果引入的是 index.js，可以使用下面的简写，等价于 import router from './router/index.js'
 import router from './router'
+import './directives'
 
 Vue.config.productionTip = false
 
