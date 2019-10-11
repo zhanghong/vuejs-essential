b79e35ba48726fdaa8d55cb2be536bea9962f509
zhanghongbean <zhanghong_bean@126.com>
2019-10-11 16:14:20 +0800

    发布个人文章

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import ls from '../utils/localStorage'
 import router from '../router'
+// 引入 actions.js 的所有导出
+import * as moreActions from './actions'
 
 Vue.use(Vuex)
 
@@ -9,7 +11,9 @@ const state = {
   // 用户信息，初始值从本地 localStorage 获取
   user: ls.getItem('user'),
   // 添加 auth 来保存当前用户的登录状态
-  auth: ls.getItem('auth')
+  auth: ls.getItem('auth'),
+  // 所有文章状态
+  articles: ls.getItem('articles')
 }
 
 const mutations = {
@@ -23,6 +27,11 @@ const mutations = {
   UPDATE_AUTH(state, auth) {
     state.auth = auth
     ls.setItem('auth', auth)
+  },
+  // 更改所有文章的事件类型
+  UPDATE_ARTICLES(state, articles) {
+    state.articles = articles
+    ls.setItem('articles', articles)
   }
 }
 
@@ -52,7 +61,10 @@ const actions = {
 
     // 更新个人信息
     commit('UPDATE_USER', user)
-  }
+  },
+
+  // 使用对象展开运算符混入 moreActions
+  ...moreActions
 }
 
 const store = new Vuex.Store({
